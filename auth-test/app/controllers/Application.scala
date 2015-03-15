629612fb4cdbad42aa7ab084c8e1fabb45010efc
Nick Shaw <nick.shaw@g4me.co.uk>
2015-03-15 20:41:52 +0000

    changes to auth-test controller

--- auth-test/app/controllers/Application.scala
+++ auth-test/app/controllers/Application.scala
@@ -6,13 +6,18 @@ import org.pac4j.play._
 import org.pac4j.play.scala._
 import play.api.libs.json.Json
 import org.pac4j.cas.profile._
+import uk.co.g4me.cas.controllers.SecureController
 
-object Application extends ScalaController {
+object Application extends SecureController {
 
-  def index = RequiresAuthentication("CasClient") { profile =>
-    Action { request =>
-      Ok(views.html.main("CAS Authentication Test App", views.html.tests(profile.asInstanceOf[CasProfile])))
-	}
+//  def index = AuthenticatedAction {
+//    Action { request =>
+//      //Ok(views.html.main("CAS Authentication Test App", views.html.tests()))
+//	}
+//  }
+  
+  def index = AuthenticatedAction {
+    Ok(views.html.main("CAS Authentication Test App", views.html.tests()))
   }
 
 }
