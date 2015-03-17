b3435afbc96df8e14d612f991472cfc284280b85
Nick Shaw <nick.shaw@g4me.co.uk>
2015-03-17 22:56:58 +0000

    fixing cast issues

--- auth-test/app/controllers/Application.scala
+++ auth-test/app/controllers/Application.scala
@@ -9,12 +9,13 @@ import org.pac4j.cas.profile._
 import uk.co.g4me.cas.controllers.SecureController
 import uk.co.g4me.cas.security.SecureHandler
 import security.CustomHandler
+import be.objectify.deadbolt.scala.DeadboltHandler
 
 object Application extends SecureController {
   
   def index = AuthenticatedAction { implicit request =>
     
-    Ok(views.html.main("CAS Authentication Test App", views.html.tests(handler)))
+    Ok(views.html.main("CAS Authentication Test App", views.html.tests(handler.asInstanceOf[DeadboltHandler])))
     
   }
 
